81a7f3d8b2afb8b18755bdccb2b3d762e7680e50
Bill <castillobil@gmail.com>
2015-11-02 21:18:24 +0400

    Updating style 2.0

--- js/main.js
+++ js/main.js
@@ -1,26 +1,27 @@
-//ANCHOR SMOOTH SCROLL
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+// WOW JS intiialization
+
+var wow = new WOW(
+  {
+    boxClass:     'animate',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null // optional scroll container selector, otherwise use window
+  }
+);
+wow.init();
+
+/* WOW JS Animate
+ * Set offset to bottom of the element  
+
+$(".animate").each(function() {
+  var wowHeight = $(this).height();
+  $(this).attr("data-wow-offset", wowHeight);
 });
 
-//WOW JS
-wow = new WOW(
-{
-  boxClass:     'animate',      // default
-  animateClass: 'animated', // default
-  offset:       -50,          // default
-  mobile:       true,       // default
-  live:         true       // default
-})
-wow.init();
+*/
