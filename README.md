db1ed0d6b7e864e8d735b058787b9e94622ef018
Bill <castillobil@gmail.com>
2016-10-15 19:56:48 +0400

    Port to jekyll

--- README.md
+++ README.md
@@ -1,15 +1,3 @@
-# Personal Portfolio / Website
+# Bill Castillo
 
-Used tools:
-  - [Jekyll assets](https://github.com/jekyll/jekyll-assets)
-  	- UPDATE: not using jekyll anymore
-  - [Bower](http://bower.io/)
-  - [Jekyll](http://jekyllrb.com/)
-  - Influences from [HTML5 Boilerplate](https://html5boilerplate.com/)
-  - [Autoprefixer](https://github.com/postcss/autoprefixer)
-
-## Deploy to GitHub Pages
-
-Run this in the root project folder in your console:
-
-    bin/deploy
+## Portfolio
--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,67 @@
-// Include gulp
-var gulp = require('gulp'); 
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
+var prefix      = require('gulp-autoprefixer');
+var cp          = require('child_process');
 
-// Include Our Plugins
-var stylus = require('gulp-stylus');
-var browserSync = require('browser-sync').create();
+var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
 
-
-// Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch('./lib/stylus/*.styl', ['stylus']);
+/**
+ * Build the Jekyll Site
+ */
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+        .on('close', done);
 });
 
-// Gulp Stylus
-gulp.task('stylus', function () {
-  gulp.src('./lib/stylus/*.styl')
-    .pipe(stylus())
-    .pipe(gulp.dest('./css'));
+/**
+ * Rebuild Jekyll & do page reload
+ */
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
 });
 
-gulp.task('browser-sync', function() {
-    browserSync.init({
+/**
+ * Wait for jekyll-build, then launch the Server
+ */
+gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+    browserSync({
         server: {
-            baseDir: "./"
+            baseDir: '_site'
         }
     });
 });
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['stylus'], function() {
-
-    browserSync.init({
-        server: "./"
-    });
-
-    gulp.watch("./lib/stylus/*.styl", ['stylus']).on('change', browserSync.reload);
-    gulp.watch("./dist/js/*.js", ['scripts']).on('change', browserSync.reload);
-    gulp.watch("./*.html").on('change', browserSync.reload);
+/**
+ * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
+ */
+gulp.task('sass', function () {
+    return gulp.src('_scss/main.scss')
+        .pipe(sass({
+            includePaths: ['scss'],
+            onError: browserSync.notify
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('_site/css'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('css'));
 });
 
-// Default Task
-gulp.task('default', ['stylus', 'serve', 'watch']);
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
+gulp.task('watch', function () {
+    gulp.watch('_scss/*.scss', ['sass']);
+    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+});
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['browser-sync', 'watch']);
